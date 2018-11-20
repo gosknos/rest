b8f800bcc08c7cdb2bda1039b9429cf339fb18cc
gosknos <gxcharlotte@gmail.com>
2018-11-20 10:50:16 +0100

    4

--- index.php
+++ index.php
@@ -3,7 +3,17 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
+class MyDB extends SQLite3 {
+    function __construct() {
+        $this->open('friends.db');
+    }
+}
 
+$db = new MyDB();
+if(!$db) {
+    echo $db->lastErrorMsg();
+    exit();
+}
 $app = new \Slim\App;
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
@@ -11,4 +21,16 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+$app->get(
+    '/friends',
+    function (Request $request, Response $response, array $args) use ($db) {
+        $sql = "select * from friend";
+        $ret = $db->query($sql);
+        $friends = [];
+        while ($friend = $ret->fetchArray(SQLITE3_ASSOC)) {
+            $friends[] = $friend;
+        }
+        return $response->withJson($friends);
+    }
+);
 $app->run();
